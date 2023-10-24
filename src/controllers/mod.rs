65c7abba011704aabb11c5e230afa0dc5b2f4422
headiron <i@liangzhicheng.cc>
2023-10-24 08:53:37 +0800

    feat: deploy (#5)

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,1 +1,36 @@
 pub(crate) mod test;
+
+pub async fn not_found() -> String {
+    "Not found".into()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::routes::router;
+    use axum::http::{self, StatusCode};
+    use hyper::{Body, Request};
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn test_test_post() {
+        let router = router();
+
+        let response = router
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .uri("/not-found")
+                    .body(Body::from("Not found"))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body = String::from_utf8(body.to_vec()).unwrap();
+
+        assert_eq!(body, "Not found");
+    }
+}
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,10 +1,14 @@
 use axum::{routing::post, Router};
 use tower_http::trace::TraceLayer;
 
-use crate::controllers::test::{test_get, test_post};
+use crate::controllers::{
+    not_found,
+    test::{test_get, test_post},
+};
 
 pub fn router() -> Router {
     Router::new()
         .route("/test", post(test_post).get(test_get))
+        .fallback(not_found)
         .layer(TraceLayer::new_for_http())
 }
